daa55aee9c5daac2a8e9bd06282977f23c15e635
Vansh <vanshisawesomer@gmail.com>
2025-11-02 22:38:33 -0500

    Finished EmptyFileException and Invalid Stopword, used minimal code and super, must write Toosmalltext and wordCounter

---
+++ EmptyFileException.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class EmptyFileException extends IOException {
+    
+    public EmptyFileException(String message) {
+        
+        super(message);
+    }
+}
---
+++ InvalidStopwordException.java
@@ -0,0 +1,8 @@
+public class InvalidStopwordException extends Exception {
+
+    public InvalidStopwordException(String message) {
+
+        super(message);
+        
+    }
+}
